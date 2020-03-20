cf8efb710caade02a263715b9a6176781364ee37
Lukman Munaji <mhanchini@gmail.com>
2020-03-21 00:37:11 +0700

    validation_1

--- src/main/java/id/agile/web/ProjectController.java
+++ src/main/java/id/agile/web/ProjectController.java
@@ -1,15 +1,19 @@
 package id.agile.web;
 
 import id.agile.domain.Project;
+import id.agile.service.MapValidationErrorService;
 import id.agile.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,6 +22,9 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject
             (
@@ -25,9 +32,9 @@ public class ProjectController {
                     @RequestBody Project project,
                     BindingResult result
             ) {
-        if (result.hasErrors()) {
-            return ResponseEntity.status(444).body("Invalid Objects");
-        }
+        ResponseEntity<?> responseEntity = mapValidationErrorService.mapValidationService(result);
+        if (responseEntity != null) return responseEntity;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
