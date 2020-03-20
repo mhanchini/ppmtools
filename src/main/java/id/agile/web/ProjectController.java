ac59caf8719191adb938b6e19d5d5f2344827b28
Lukman Munaji <mhanchini@gmail.com>
2020-03-21 00:09:12 +0700

    change_validation

--- src/main/java/id/agile/web/ProjectController.java
+++ src/main/java/id/agile/web/ProjectController.java
@@ -5,7 +5,10 @@ import id.agile.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,10 +18,15 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject
+    public ResponseEntity<?> createNewProject
             (
-                    @RequestBody Project project
+                    @Valid
+                    @RequestBody Project project,
+                    BindingResult result
             ) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Objects!", HttpStatus.valueOf(404));
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
